20bff93b6a9f10be7989bf98387267232e4c3310
Music-33 <112193400+Music-33@users.noreply.github.com>
2023-06-17 20:51:48 +0100

    Update zimbot.js
    Signed-off-by: Music-33 <112193400+Music-33@users.noreply.github.com>

--- zimbot.js
+++ zimbot.js
@@ -8,7 +8,7 @@
 //▮
 //▮REPORT ABUSE OF THIS SOFTWARE EMAIL US
 //▮reinhardtuna@mail.uk
-//▮WHATSAPP US : +44 7441 437150
+//▮WHATSAPP US : +212774339435
 //▮YOUTUBE CHANNELL: https://youtube.com/c/DRIPSOFC
 //▮
 //╰▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
